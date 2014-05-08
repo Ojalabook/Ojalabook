e0c7d046562cd337a61749f27c42d5d1c7b91b17
carlos <carlosj.gonzalez28@gmail.com>
2014-05-08 16:18:43 -0430

    haciendo pruebas en la aplicacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ActiveRecord::Base
   attr_accessible :primer_nombre, :primer_apellido, :nombre_perfil,
   				  :email, :password, :password_confirmation
 
-  has_many :statuses				  
+  has_many :statuses
 
+  validates :primer_nombre, presence: true
+
+  validates :primer_apellido, presence: true
+
+  validates :nombre_perfil, presence: true,				  
+                            uniqueness: true,
+                            format: {
+                              with: /a-zA-Z0-9_-/,
+                              message:'Debe estar formateado correctamente.'
+                            }      
   def nombre_completo
   	primer_nombre + " " + primer_apellido
   end				  				       
