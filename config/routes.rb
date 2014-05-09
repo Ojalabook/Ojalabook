1c08fce06919d563b3495cd3e5033a7cff2a854f
carlos <carlosj.gonzalez28@gmail.com>
2014-05-09 15:30:59 -0430

    Nuevas direcciones en el proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Ojalabook::Application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'registrar', to: 'devise/registrations#new', as: :registrar
+    get 'comenzar', to: 'devise/sessions#new', as: :comenzar
+    get 'destruir', to: 'devise/sessions#destroy', as: :destruir
+  end
+
   resources :statuses
+  get 'timeline', to: 'statuses#index', as: :timeline
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
